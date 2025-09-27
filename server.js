91c413dd651c2b577a6bf1cc439e39e03b461a09
Daniel obinna <dobi44134@gmail.com>
2025-09-27 03:33:32 +0100

    hosting

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const app = express()
 app.use(express.json())
 app.use(userRouter)
 
+app.get('/', (req, res) => {
+  res.send('🚀 SnapBreakfast API is running on Vercel!')
+})
+
 module.exports = app
