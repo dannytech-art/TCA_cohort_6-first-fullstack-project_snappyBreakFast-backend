97ac3cb2b3cf2c9b6613f99a01d910d2112f03ce
Daniel obinna <dobi44134@gmail.com>
2025-09-27 03:28:35 +0100

    hosting

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express')
 require('./config/database')
 const userRouter = require('./routes/userRouter')
+
 const app = express()
-const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 
-app.listen(port, ()=>{
-    console.log(`app is running on port: ${port}`)
-})
-
+module.exports = app
