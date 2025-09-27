119f19589ca58b0e03beb9493bc6bf6ef913c78a
Daniel obinna <dobi44134@gmail.com>
2025-09-27 20:44:50 +0100

    verifyOtp endpoint

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,4 @@
-const { signup, verifyOtp } = require('../controllers/userControllers')
+const { signup, verifyOtp, resendOtp } = require('../controllers/userControllers')
 const { authenticate } = require('../middleware/authentication')
 
 const router = require('express').Router()
@@ -6,4 +6,7 @@ const router = require('express').Router()
 router.post('/user/signup', signup)
 
 router.post('/user/otp', authenticate , verifyOtp)
+
+router.post('/user/resend-otp', authenticate , resendOtp)
+
 module.exports = router
