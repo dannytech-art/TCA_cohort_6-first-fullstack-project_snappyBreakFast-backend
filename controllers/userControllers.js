6f41a412884952f52e429841101ec409009117a2
Daniel obinna <dobi44134@gmail.com>
2025-09-27 01:15:40 +0100

    complete signup and verify api onboarding completed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { signUpTemp } = require("../utils/emailTemplate");
 const emailSender = require("../middleware/nodemailer");
 const generateVerificationCode = require("../middleware/otpGenerate");
-const verificationCode = generateVerificationCode()
+const { otp, expiresAt } = generateVerificationCode()
 exports.signup = async (req,res)=> {
     try {
         const { firstName, lastName, email, phoneNumber } = req.body
@@ -23,7 +23,8 @@ exports.signup = async (req,res)=> {
             email: email.toLowerCase(),
             phoneNumber: `+234${phoneNumber.slice(1)}`
         })
-
+        user.otp = otp
+        user.otpExpiry = expiresAt
         await user.save()
 
         const token = jwt.sign({
@@ -35,15 +36,52 @@ exports.signup = async (req,res)=> {
         const emailOption = {
         email: user.email,
         subject: 'Verify your email',
-        html: signUpTemp(verificationCode, user.firstName)
+        html: signUpTemp(otp, user.firstName)
     }
        
         await emailSender(emailOption)
         res.status(201).json({
             message: `Account created successfully`,
-            data: user
+            data: user,
+            token
         })
          
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+exports.verifyOtp = async (req,res)=>{
+    try {
+        const { otp } = req.body
+        const decodedId = req.user.id
+        const user = await userModel.findById(decodedId);
+        if (!user) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        if (user.otp !== String(otp)) {
+            return res.status(400).json({ message: "Invalid OTP" });
+        }
+        if (Date.now() > user.otpExpiry) {
+            return res.status(400).json({ message: `OTP expired`})
+        }
+        
+        user.isVerified = true
+        user.otp = null
+        user.otpExpiry = null
+        await user.save()
+        const token = jwt.sign({
+            id: user._id,
+            email: user.email
+        },process.env.JWT_SECRET,{expiresIn: '1d'})
+
+        res.status(200).json({
+            message: `Account verified successfully`,
+            data: user,
+            token
+
+        })
     } catch (error) {
         res.status(500).json({
             message: error.message
