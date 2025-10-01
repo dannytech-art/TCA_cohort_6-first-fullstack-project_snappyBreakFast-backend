f25e174feff223b42ae6f9135a1290e3b5855c09
codeconnoisseur-channels <channelsokunade@gmail.com>
2025-10-01 18:42:22 +0100

    cart section added

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-require('./config/database');
-const userRouter = require('./routes/userRouter');
-const resturantRouter = require('./routes/resturantRouter');
-const foodRouter = require('./routes/foodRouter');
+const express = require("express");
+const cors = require("cors");
+require("./config/database");
+const userRouter = require("./routes/userRouter");
+const resturantRouter = require("./routes/resturantRouter");
+const foodRouter = require("./routes/foodRouter");
+const cartRouter = require("./routes/cartRouter");
 // const port = process.env.PORT;
 const app = express();
 
-
-app.use(cors({
-  origin: [
-    "http://localhost:5173",             
-    "https://your-frontend.vercel.app"   
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 app.use(userRouter);
 app.use(resturantRouter);
 app.use(foodRouter);
-
+app.use(cartRouter);
 
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
 
-app.get('/', (req, res) => {
-  res.send('SnapBreakfast API is running on Vercel!');
+app.get("/", (req, res) => {
+  res.send("SnapBreakfast API is running on Vercel!");
 });
 
 module.exports = app;
