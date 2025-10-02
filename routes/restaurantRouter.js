52cc9fe8d46fd655b58ead870ef139f1af6bbca7
Daniel obinna <dobi44134@gmail.com>
2025-10-02 12:35:18 +0100

    update

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -1,4 +1,4 @@
-const { createRestaurant, getAllRestaurants, getRestaurantById } = require('../controllers/restaurantcontroller')
+const { createRestaurant, getAllRestaurants } = require('../controllers/restaurantcontroller')
 const upload = require('../middleware/multer')
 const router = require('express').Router()
 
@@ -7,4 +7,5 @@ router.post('/restaurant',upload.single('image') ,createRestaurant)
 router.get('/restaurants', getAllRestaurants)
 
 router.get('/restaurant/:id', getRestaurantById)
+
 module.exports = router
