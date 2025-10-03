41ddde40e15dab7b88218aa476d682fd48531868
Daniel obinna <dobi44134@gmail.com>
2025-10-03 13:34:33 +0100

    update

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 
-const express = require('express');
-const cors = require('cors');
-require('./config/database');
-const userRouter = require('./routes/userRouter');
-const restaurantRouter = require('./routes/restaurantRouter');
-const foodRouter = require('./routes/foodRouter');
-const cartRouter = require("./routes/cartRouter");
-const transactionRouter = require('./routes/transactionRouter');
-const port = process.env.PORT;
-const app = express();
+const express = require('express')
+const cors = require('cors')
 
-app.use(express.json());
-app.use(cors('*'));
+require('./config/database')
 
-app.use(userRouter);
-app.use(restaurantRouter);
-app.use(foodRouter);
-app.use(cartRouter);
-app.use(transactionRouter);
+const userRouter = require('./routes/userRouter')
+
+const restaurantRouter = require('./routes/restaurantRouter')
+
+const foodRouter = require('./routes/foodRouter')
+
+const cartRouter = require("./routes/cartRouter")
+
+const transactionRouter = require('./routes/transactionRouter')
+
+const port = process.env.POR
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors('*'))
+
+app.use(userRouter)
+
+app.use(restaurantRouter)
+
+app.use(foodRouter)
+
+app.use(cartRouter)
+
+app.use(transactionRouter)
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
 
 app.get("/", (req, res) => {
-  res.send("SnapBreakfast API is running on Vercel!");
-});
+  res.send("SnapBreakfast API is running on Vercel!")
+})
 
 module.exports = app;
